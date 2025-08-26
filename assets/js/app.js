441875c1b777864792cf5e45c7c1aa3dcc2400c8
Rodo999 <rodolfoalexis45@gmail.com>
2025-08-25 22:50:34 -0600

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -218,13 +218,8 @@
         <div>
           <h4>Contacto</h4>
           <p class="m-0">WhatsApp: <a href="https://wa.me/527226128347" target="_blank" rel="noopener">+52 722 612 8347</a></p>
-<p class="m-0">
-  Correo: 
-  <a href="mailto:rodolfoalexis45@gmail.com,danielamore1151@gmail.com?subject=🌸%20Nuevo%20Cliente%20-%20Lovelane%20Meleara&body=👤%20Nombre%20del%20cliente:%0D%0A📞%20Teléfono:%0D%0A📧%20Correo:%0D%0A🎁%20Producto/Servicio%20de%20interés:%0D%0A💰%20Precio%20cotizado:%0D%0A💳%20Anticipo%20(%25):%0D%0A📅%20Fecha%20de%20entrega:%0D%0A📝%20Notas%20adicionales:%0D%0A%0D%0AGracias,%20quedo%20atento%20a%20confirmación%20🌟">
-    ✉️ Enviar Presentación de Cliente
-  </a>
-</p>
-          <p class="m-0">Pagos: Stripe, MercadoPago y Transferencia</p>
+<p class="m-0">Correo: <a href="mailto:rodolfoalexis45@gmail.com,danielamore1151@gmail.com?subject=🌸%20Nuevo%20Cliente%20-%20Lovelane%20Meleara&body=👤%20Nombre%20del%20cliente:%0D%0A📞%20Teléfono:%0D%0A📧%20Correo:%0D%0A🎁%20Producto/Servicio%20de%20interés:%0D%0A💰%20Precio%20cotizado:%0D%0A💳%20Anticipo%20(%25):%0D%0A📅%20Fecha%20de%20entrega:%0D%0A📝%20Notas%20adicionales:%0D%0A%0D%0AGracias,%20quedo%20atento%20a%20confirmación%20🌟">✉️ Enviar Presentación de Cliente</a></p>
+          <p class="m-0">Pagos: En tarjeta, Depositos, MercadoPago y Transferencia</p>
         </div>
       </div>`;
   }
